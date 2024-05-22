f552d65ea50e1e0e0701c782ada843fd30adefd7
cr4zyfish <90797624+cr4zyfish@users.noreply.github.com>
2024-05-22 18:16:26 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-go install github.com/tc-hib/go-winres@latest
+go install github.com/tc-hib/go-winres@v0.3.3
 go-winres simply --icon icon.png
 go build -o TrayApp.exe -ldflags -H=windowsgui
 echo Finished! Press any key to continue...
